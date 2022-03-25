5690e09610e8c27b02b20775ace95f8336d6b797
igarralmat <93262344+igarralmat@users.noreply.github.com>
2022-03-25 14:18:49 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Happy)
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
-        basic.pause(2100)
+        basic.pause(2000)
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
-        basic.pause(500)
+        basic.pause(580)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
         DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 40)
         DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
-        basic.pause(590)
+        basic.pause(580)
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
         basic.pause(500)
     }
